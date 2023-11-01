9356ab7385b8868b6548299f96c8c714cf75d096
Sumanth <Sumanthts2930@gmail.com>
2023-10-31 21:35:14 -0400

    Create Assignment_2.cpp
    added second Assignment

---
+++ Assignment_2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+struct Sparse
+{
+    int NumOfRows, NumOfCols, valCount;
+    int **nonZeroData; 
+};
+
+void addNumber(Sparse &matrix, int row, int col, int value)
+{
+    if (value != 0)
+    {
+        matrix.nonZeroData[matrix.valCount][0] = row;
+        matrix.nonZeroData[matrix.valCount][1] = col;
+        matrix.nonZeroData[matrix.valCount][2] = value;
+        matrix.valCount++;
+    }
+}
+
+int getNumber(Sparse &matrix, int row, int col)
+{
+    for (int i = 0; i < matrix.valCount; i++)
+    {
+        if (matrix.nonZeroData[i][0] == row && matrix.nonZeroData[i][1] == col)
+        {
+            return matrix.nonZeroData[i][2];
+        }
+    }
+    return 0; 
+}
+
+void printMatrix(Sparse &matrix)
+{
+    std::cout << "Sparse Matrix:" << std::endl;
+    for (int i = 0; i < matrix.NumOfRows; i++)
+    {
+        for (int j = 0; j < matrix.NumOfCols; j++)
+        {
+            std::cout << getNumber(matrix, i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    int rows, cols;
+    std::cout << "Enter the total number of rows: ";
+    std::cin >> rows;
+    std::cout << "Enter the total number of columns: ";
+    std::cin >> cols;
+
+    Sparse matrix;
+    matrix.NumOfRows = rows;
+    matrix.NumOfCols = cols;
+    matrix.valCount = 0;
+
+ 
+    matrix.nonZeroData = new int *[rows * cols];
+    for (int i = 0; i < rows * cols; i++)
+    {
+        matrix.nonZeroData[i] = new int[3]; 
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            int value;
+            std::cout << "Enter value for element at row " << i << " and column " << j << ": ";
+            std::cin >> value;
+            addNumber(matrix, i, j, value);
+        }
+    }
+
+    printMatrix(matrix);
+
+    // de allocating memory
+    for (int i = 0; i < matrix.NumOfRows; i++)
+    {
+        delete[] matrix.nonZeroData[i];
+    }
+    delete[] matrix.nonZeroData;
+
+    return 0;
+}
